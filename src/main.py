a1685c9e9d07831f14286078fc583e03c1bbd617
Alexander Röhl <alexander.roehl@student.uni-tuebingen.de>
2019-12-14 09:51:09 +0100

    Upload JSpeciesWS files + Add commandline check

--- src/main.py
+++ src/main.py
@@ -3,6 +3,18 @@ import sys
 
 
 @click.command()
-@click.option("-i", "--input-filepath", default="data")
-def main():
+@click.option("-i", "--input-filepath", nargs=2, default="data")
+@click.option("-fasta/-nfasta", "--collect-fastafile/--ncollect-fastafile", default=False)
+@click.option("-of", "--output-fastapath", default='')
+def main(input_filepath, collect_fastafile, output_fastapath):
+    if check_commandline(input_filepath, collect_fastafile, output_fastapath):
+        print()
     sys.exit()
+
+
+def check_commandline(input_filepath, collect_fastafile, output_fastapath):
+    if (collect_fastafile and output_fastapath) or (not collect_fastafile):
+        return True
+    else:
+        print("Error: If a fasta-file should be collected, please provide an outputpath.")
+        return False
