be3dcac8ee607871152ca8f9253b458c603908cf
Alexander Röhl <alexander.roehl@student.uni-tuebingen.de>
2019-12-14 09:55:23 +0100

    Add some standard console outputs

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 import click
 import sys
 
+__author__ = "Nantia Leonidou, Florian Riedl, Alexander Röhl"
+
 
 @click.command()
 @click.option("-i", "--input-filepath", nargs=2, default="data")
 @click.option("-fasta/-nfasta", "--collect-fastafile/--ncollect-fastafile", default=False)
 @click.option("-of", "--output-fastapath", default='')
 def main(input_filepath, collect_fastafile, output_fastapath):
+    print("Running Project Tasks")
+    print("Authors: ", __author__, '\n')
     if check_commandline(input_filepath, collect_fastafile, output_fastapath):
         print()
+    print("Exit")
     sys.exit()
 
 
